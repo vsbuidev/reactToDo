fb70f4009153a415e643c5784ffeae04d348bcac
vsbuidev <vsb.uidev@gmail.com>
2024-02-07 13:37:53 +0530

    styles and scripts are added, next Figma styles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faCheck, faTrash } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
 
 function App() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
 
-  const addTask = () => {
+  const addTask = (event) => {
     if (newTask.trim() !== "") {
-      setTasks([...tasks, newTask]);
+      setTasks([...tasks, { text: newTask, completed: false }]);
       setNewTask("");
+      event.preventDefault();
+    } else {
+      alert("Task cannot be empty!");
+      event.preventDefault();
     }
   };
 
   const editTask = (index, updatedTask) => {
     const updatedTasks = [...tasks];
-    updatedTasks[index] = updatedTask;
+    updatedTasks[index].text = updatedTask;
+    setTasks(updatedTasks);
+  };
+
+  const checkTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed;
     setTasks(updatedTasks);
   };
 
@@ -27,28 +39,35 @@ function App() {
   return (
     <div className="App">
       <h1>Todo List</h1>
-      <div>
+      <form>
         <input
           type="text"
           placeholder="Add a new task"
           value={newTask}
           onChange={(e) => setNewTask(e.target.value)}
         />
-        <button className="add" onClick={addTask}>
-          Add task
+        <button type="submit" className="add" onClick={addTask}>
+          <FontAwesomeIcon icon={faPlus} />
         </button>
-      </div>
+      </form>
       <ul>
         {tasks.map((task, index) => (
           <li key={index}>
             <input
               type="text"
-              value={task}
+              value={task.text}
               onChange={(e) => editTask(index, e.target.value)}
-              className="li-input"
+              className={task.completed ? "li-input-completed" : "li-input"}
+              disabled={task.completed}
             />
+            <button
+              className={task.completed ? "checked" : "check"}
+              onClick={() => checkTask(index)}
+            >
+              <FontAwesomeIcon icon={faCheck} />
+            </button>
             <button className="remove" onClick={() => removeTask(index)}>
-              Remove
+              <FontAwesomeIcon icon={faTrash} />
             </button>
           </li>
         ))}
